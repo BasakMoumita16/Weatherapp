a8954eb1d7f269e9d22f7ac70feedaf52f9d211f
crissesoto <soto.crisse@hotmail.com>
2019-11-18 16:16:59 +0100

    added API

---
+++ Dist/forecast.js
@@ -0,0 +1,19 @@
+const APIkey = "cbf7a62a8ad5eb0729fd2051f6515821";
+
+const getCity = async cityName => {
+  // we make a request passing the API call and the parameter
+  //we get from the function we put in as que q = querie in to
+  // the API call
+  const APIcall = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${APIkey} `;
+
+  // response is the response que get once we fetch from the
+  //API CALL
+  const response = await fetch(APIcall);
+
+  // We format the response into data using json
+  const data = await response.json();
+
+  console.log(data);
+};
+
+getCity("delhi");
